a75aff7405fb8749b8952694e3839c5f311cea14
omg <s24018@std.it-college.ac.jp>
2025-11-13 10:40:09 +0900

    現時点最終コミット

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,25 @@
-import './globals.css';
-import Header from './_components/Header';
-import Footer from './_components/Footer';
+import "./globals.css";
+import type { Metadata } from "next";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
+export const Metadata: Metadata = {
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description:
+    "「Next.js + ヘッドレスCMSではじめる！ かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description:
+      "「Next.js + ヘッドレスCMSではじめる！ かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    cononical: "http://localhost:3000/",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
